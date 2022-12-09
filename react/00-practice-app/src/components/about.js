cf2a5ef46a1d3b11cadd5dd5ce1c09bf4d1a7228
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-08 18:36:32 -0600

    filled out navbar

---
+++ react/00-practice-app/src/components/about.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function About() {
+  <section id="about">
+    <div className="container px-5 py-10 mx-auto text-center lg:px-40">
+      <div className="flex flex-col w-full mb-20">
+        <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-white">
+          About Me: My name is Angela Amaro and I am a full stack web developer.
+          I am currently enrolled in the UCB Coding Bootcamp. Welcome to my
+          portfolio! Im from the Bay Area and I love to code! Here you can see
+          past projects I've worked on and there is a contact form if you would
+          like to reach out to me. I've also included my resume.
+        </h1>
+      </div>
+    </div>
+  </section>;
+}
+
+export default About;
