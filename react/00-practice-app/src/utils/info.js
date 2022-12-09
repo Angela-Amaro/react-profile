b6f71386420b57afa9c358894bf358e6c248d112
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-08 19:21:17 -0600

    populated projects in projects.js

--- react/00-practice-app/src/utils/info.js
+++ react/00-practice-app/src/utils/info.js
@@ -1,59 +1,60 @@
-//TODO: fill out projects
 export const projects = [
-     {
-        title: "Project 1", 
-        description: "",
-        image: "",
-        link: "",
-    },
-    {
-        title: "Project 2",
-        description: "",    
-        image: "",
-        link: "",
-    },
-    {
-        title: "Project 3",
-        description: "",
-        image: "",
-        link: "",
-    },
-    {
-        title: "Project 4",
-        description: "",
-        image: "",
-        link: "",
-    },
-
-]
+  {
+    title: "Project 1",
+    description:
+      "welcome to my weather dashboard. Input any city you want to see the current weather and a 5 day forecast! You can also see your past searches as well as re-search them by clicking on them in the past searches bank.",
+    image: "./weatherrecord.gif",
+    link: "https://github.com/Angela-Amaro/weather-dashboard-app",
+  },
+  {
+    title: "Project 2",
+    description:
+      "A quick and efficient note taking app. Here you can take class notes and save it, then you can see your note saved on the list to the left!",
+    image: "./notetaker.jpg",
+    link: "https://github.com/Angela-Amaro/note-takin-app",
+  },
+  {
+    title: "Project 3",
+    description:
+      "welcome to my password generator application! Here you can generate a safe and secure password based on your criteria. In this application you can have a password generated between 8-129 characters long! you can generate a password with numbers, uppercase letters, lowercase letters, and special characters.",
+    image: "./gif_of_pass_generator.gif",
+    link: "https://github.com/Angela-Amaro/password-generator",
+  },
+  {
+    title: "Project 4",
+    description:
+      "Feel free to take this quiz! hint: they're coding related questions.",
+    image: "./quiz.png",
+    link: "https://github.com/Angela-Amaro/quiz",
+  },
+];
 
 export const skills = [
-{
+  {
     title: "HTML",
-},
-{
+  },
+  {
     title: "CSS",
-},
-{
+  },
+  {
     title: "JavaScript",
-},
-{
+  },
+  {
     title: "React",
-},
-{
+  },
+  {
     title: "Node.js",
-},
-{
+  },
+  {
     title: "Express.js",
-},
-{
+  },
+  {
     title: "MongoDB",
-},
-{
+  },
+  {
     title: "MySQL",
-},
-{
+  },
+  {
     title: "sequelize",
-},
-
-]
+  },
+];
