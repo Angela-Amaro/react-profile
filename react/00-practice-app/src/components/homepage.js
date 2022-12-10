521295136f4880c5e9952cc0e7336083843d684b
Angela-Amaro <aamaro@alumnae.mills.edu>
2022-12-10 14:35:07 -0600

    remnamed components

--- react/00-practice-app/src/components/homepage.js
+++ react/00-practice-app/src/components/homepage.js
@@ -1,38 +1,36 @@
-import React, {useState} from "react";
-import Footer from "./footer";
-import About from "./about";
-import Navbar from "./navbar";
-import Contact from "./contact";
-import Resume from "./resume";
-import Projects from "./projects";
+import React, { useState } from "react";
+import Footer from "./Footer";
+import About from "./About";
+import Navbar from "./Navbar";
+import Contact from "./Contactform";
+import Resume from "./Resume";
+import Projects from "./Projects";
 
 function Homepage() {
-    const [currentPage, handlePageChange ] = useState("About");
-    
-    const renderPage = () => {
-        if (currentPage === "About") {
-        return <About />;
-        }
-        if (currentPage === "Contact") {
-        return <Contact />;
-        }
-        if (currentPage === "Resume") {
-        return <Resume />;
-        }
-        if (currentPage === "Projects") {
-        return <Projects />;
-        }
-    };
-    
-    
-    
-    return (
-        <div>
-        <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
-        <div>{renderPage(currentPage)}</div>
-        <Footer />
-        </div>
-    );
+  const [currentPage, handlePageChange] = useState("About");
+
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
     }
+    if (currentPage === "Contact") {
+      return <Contact />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    if (currentPage === "Projects") {
+      return <Projects />;
+    }
+  };
+
+  return (
+    <div>
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      <div>{renderPage(currentPage)}</div>
+      <Footer />
+    </div>
+  );
+}
 
 export default Homepage;
